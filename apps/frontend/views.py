6af8f472adb60502ad8323f66f81a77143fc4919
Ivan Maldonado <ivanmaldonaxdo98@gmail.com>
2022-10-31 20:59:01 -0300

    pequeños cambios

--- apps/frontend/views.py
+++ apps/frontend/views.py
@@ -11,40 +11,38 @@ def processDocs(request):
     # render_vacio = render(request, 'frontend/inicio.html')  
     q_boton = request.GET.get('btnbuscar')
     print("FOLIO => {}| TSERVICIO => {} | Boton => {}" .format( q_folio,q_tpServicio,q_boton))
-    if q_folio == None and  q_tpServicio == "Tipo de servicio":
-        return render(request, 'frontend/inicio.html')  
-    else:    
-        if q_tpServicio == "Tipo de servicio":
-            q_tpServicio = None
-        print("BUSQUEDA => FOLIO: {} - Tipo Servicio: {}".format(q_folio,q_tpServicio) )
-        scheme = request.scheme               # http or https
-        meta = request.META['HTTP_HOST']
-        # print("Dominio => scheme: {} - meta Servicio: {}".format(scheme,meta) )
+    # if q_folio == None and  q_tpServicio == "Tipo de servicio":
+    #     return render(request, 'frontend/inicio.html')  
+    # else:    
+    #     if q_tpServicio == "Tipo de servicio":
+    #         q_tpServicio = None
+    #     print("BUSQUEDA => FOLIO: {} - Tipo Servicio: {}".format(q_folio,q_tpServicio) )
+    #     scheme = request.scheme               # http or https
+    #     meta = request.META['HTTP_HOST']
+    #     # print("Dominio => scheme: {} - meta Servicio: {}".format(scheme,meta) )
         
-        # domain = Site.objects.get_current().domain
-        # print(domain)
-        url = 'http://127.0.0.1:8000/documentos/search_docs/'
-        data = {
-                "folio":q_folio,
-                "tipo_servicio":q_tpServicio,
-                "rut_receptor":None
-            }
-        data = dict(data)
-        headers = {'Accept': 'application/json'}
-        response = requests.post(url,json = data,headers = headers)
-        print("")
+    #     # domain = Site.objects.get_current().domain
+    #     # print(domain)
+    #     url = 'http://127.0.0.1:8000/documentos/search_docs/'
+    #     data = {
+    #             "folio":q_folio,
+    #             "tipo_servicio":q_tpServicio,
+    #             "rut_receptor":None
+    #         }
+    #     data = dict(data)
+    #     headers = {'Accept': 'application/json'}
+    #     response = requests.post(url,json = data,headers = headers)
+    #     print("")
         
-        # boletas = response.json() if 'detail' not in response.json() else None
-        procesados = response.json() 
-        print(type(procesados))
-        # print(boletas)
-        # q_tpServicio = "Tipo de servicio"
-        context = {
-           'procesados':procesados
-        }
-        return render(request, 'frontend/inicio.html',context)  
-
-
+    #     # boletas = response.json() if 'detail' not in response.json() else None
+    #     procesados = response.json() 
+    #     print(type(procesados))
+    #     # print(boletas)
+    #     # q_tpServicio = "Tipo de servicio"
+    #     context = {
+    #        'procesados':procesados
+    #     }
+    #     # return render(request, 'frontend/inicio.html',context)  
     return render(request, 'frontend/inicio.html')  
 
 def login(request):
