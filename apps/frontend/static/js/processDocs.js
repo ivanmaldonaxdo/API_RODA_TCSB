481103bc0bcfb13c3118c4e5a3ae62ebf87333fa
Ivan Maldonado <ivanmaldonaxdo98@gmail.com>
2022-11-01 00:39:37 -0300

    La data se inserta a tabla con el js

--- apps/frontend/static/js/processDocs.js
+++ apps/frontend/static/js/processDocs.js
@@ -16,34 +16,64 @@ function getCookie(cname) {
 var csrftoken = getCookie('csrftoken');
 ////////
 
-let folio = 11419589
-let tpServicio = null
-let rut = null
+// let folio = 11419589
+// let tpServicio = null
+// let rut = null
 document.getElementById("processDocs").addEventListener('click', function (e) {
-    console.log("hola chavales");
-    // getUser()
+    let folio = document.getElementById("folio").value,
+        servicio = document.getElementById("tipo_servicio").value
+    e.preventDefault()
+    console.log(folio)
+    console.log(servicio);
+    if (servicio =="Tipo de servicio"  && folio == ""){
+        console.log("NADA DE INFO");
+    }
+    else{
+        if (servicio == "Tipo de servicio") {
+            // console.log("servicio inservible");
+
+            servicio = null
+        }
+        getDocs(folio, servicio)
+    }
 })
 
 
 
-// var url = 'http://localhost:8000/documentos/search_docs/'
-// function getUser() {
-//     fetch(url, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'X-CSRFToken': csrftoken,
-//         },
-//         body: JSON.stringify({
-//             "folio": folio,
-//             "tipo_servicio": tpServicio,
-//             "rut_receptor": rut
-//         })
+var url = 'http://localhost:8000/documentos/search_docs/'
+function getDocs(folio,tpServicio, rutCli = null ) {
+    // const HTMLResponse = document.querySelector("#tablaJS")
+    const tbody = document.querySelector("#tablaJS")
+    const tr = document.createElement("tr")
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body: JSON.stringify({
+            "folio": folio,
+            "tipo_servicio": tpServicio,
+            "rut_receptor": rutCli
+        })
 
-//     })
-//     .then((response) => {
-//         response.json().then(data => {
-//             console.log(data)
-//         })
-//     });
-// }
+    })
+    .then((response) => {
+        response.json().then(docs => {
+            let body = ''
+            docs.map((doc) => {
+                let tdfolio = `<td class = "centrado">${doc.folio}</td>`,
+                    tdnomDoc = `<td>${doc.nomDoc}</td>`,
+                    tdRutReceptor = `<td>${doc.rut_receptor}</td>`,
+                    tdTpServicio = `<td>${doc.tipo_servicio}</td>`;
+                    // let elem = document.createElement('td')
+                    // elem.appendChild(document.createTextNode(`${doc.folio}`))
+                    // tr.appendChild(elem)
+                body += `<tr>${tdfolio}${tdnomDoc}${tdRutReceptor}${tdTpServicio}</tr>`;
+                tbody.innerHTML += body;
+            }
+            );
+            
+        })
+    });
+}
