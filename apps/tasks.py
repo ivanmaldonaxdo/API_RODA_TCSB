3bc2ea1e6dd9c7744b905b5c09cc417318304d54
Carlfv <fabi.ignaciocx@gmail.com>
2022-11-24 01:47:41 -0300

    Cron act

--- apps/tasks.py
+++ apps/tasks.py
@@ -1,6 +1,6 @@
 from django_cron import CronJobBase, Schedule
 import requests
-from apps.management.models import ConfigCron
+from apps.management.models import ConfigCron, Servicio
 from datetime import datetime
 from django import forms
 from apps.management.cron.serializers import SistemaSerializers
@@ -10,43 +10,40 @@ import time
 class MyCronJob(CronJobBase):
     cronconfig=ConfigCron.objects.get(id=1)
     cursor = cronconfig.cursor
-
-    servicios = ['AGU','ELE','GAS']
-
+    servicio = Servicio.objects.get(id=cursor)
     data=SistemaSerializers(cronconfig)
-    luz=data.data['hora_luz']
-    agua=data.data['hora_agua']
-    gas=data.data['hora_gas']
-    RUN_EVERY_MINS = 2
-    #RUN_AT_TIMES = ['04:39',]
-    schedule = Schedule(run_every_mins = RUN_EVERY_MINS)
-    #schedule = Schedule(run_at_times=RUN_AT_TIMES)
+    exec=data.data['hora_exec']
+    #RUN_EVERY_MINS = 5
+    RUN_AT_TIMES = [exec]
+    #schedule = Schedule(run_every_mins = RUN_EVERY_MINS)
+    schedule = Schedule(run_at_times=RUN_AT_TIMES)
     code = 'CronProcessDocs'    # a unique code
 
     def do(self):
         if self.cronconfig.is_active == True:
             url =  'http://localhost:8000/documentos/search_docs/'
             cookie = settings.CRON_CREDENCIAL
-            body={'folio': '', 'tipo_servicio': 'GAS', 'rut_receptor': None}
+            body={'folio': '', 'tipo_servicio': self.servicio.servicio, 'rut_receptor': None}
             ConfigCron.objects.filter(id=1).update(status = 'Buscando Informacion')
             search = requests.post(url=url, json=body, cookies={'jwt':cookie})
             data = search.json()
             ConfigCron.objects.filter(id=1).update(status = 'Procesando documentos')
-            for bol in data:
-                url2 =  'http://localhost:8000/cron/verificar_status/'
-                cookie = settings.CRON_CREDENCIAL
-                estado = requests.get(url=url2, cookies={'jwt':cookie})
-                con = estado.json()
-                if con['status'] == True:
-                    uidd= bol['uuid']
-                    nomDoc = bol['nomDoc']
-                    rut_emisor = bol['rut_emisor']
-                    url3 =  'http://localhost:8000/documentos/process_docs/'
-                    body_proc= {'uuid' :uidd , "nomDoc" : nomDoc, "rut_emisor": rut_emisor}
-                    proc = requests.post(url=url3, json=body_proc, cookies={'jwt':cookie})
-                    time.sleep(5)
-                else:
-                    return 'Proceso Pausado'
+            # for bol in data:
+            #     url2 =  'http://localhost:8000/cron/verificar_status/'
+            #     cookie = settings.CRON_CREDENCIAL
+            #     estado = requests.get(url=url2, cookies={'jwt':cookie})
+            #     con = estado.json()
+            #     if con['status'] == True:
+            #         uidd= bol['uuid']
+            #         nomDoc = bol['nomDoc']
+            #         rut_emisor = bol['rut_emisor']
+            #         rut_cliente = bol['rut_client']
+            #         url3 =  'http://localhost:8000/documentos/process_docs/'
+            #         body_proc= {'uuid' :uidd , "nomDoc" : nomDoc, "rut_emisor": rut_emisor, "rut_client":rut_cliente}
+            #         proc = requests.post(url=url3, json=body_proc, cookies={'jwt':cookie})
+            #         time.sleep(5)
+            #     else:
+            #         return 'Proceso Pausado'
             ConfigCron.objects.filter(id=1).update(status = 'Finalizando')
             if self.cronconfig.cursor<2:
                 ConfigCron.objects.filter(id=1).update(cursor = self.cursor+1)
