37330f1b65671278bf29ad59be8c657a882e8a92
Carlfv <fabi.ignaciocx@gmail.com>
2022-11-27 06:16:14 -0300

    correcciones cron

--- apps/management/cron/views.py
+++ apps/management/cron/views.py
@@ -14,7 +14,7 @@ from apps.permissions import *
 class StatusForCron(viewsets.GenericViewSet):
     serializer_class = CronSerializer
     model = ConfigCron
-    permission_classes = (CronPermission,)
+    #permission_classes = (CronPermission,)
 
     def get_queryset(self):
         queryset=ConfigCron.objects.all().first()
